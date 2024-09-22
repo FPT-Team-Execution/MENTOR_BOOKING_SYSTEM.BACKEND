f4ad8cc74ecac56bd042e93333975a659fd38bbb
ThoNT16 <truongthonguyen107@gmail.com>
2024-09-22 16:21:46 +0700

    feat: init base project backend [ThoNT]

---
+++ MBS.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MBS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public string HealthCheck()
+        {
+            return "Mentor Booking System: " + DateTime.Now;
+        }
+    }
+}
