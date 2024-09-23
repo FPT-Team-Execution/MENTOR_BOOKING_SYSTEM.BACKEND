afacbf167223e2fa2f45a8515f07a844deb5f11b
DatSung <datsung.dev@gmail.com>
2024-09-23 11:19:14 +0700

    feat: set up jwt authentication

---
+++ MBS.API/Extensions/AuthenticationDependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿namespace MBS.API.Extensions
+{
+    public static class AuthenticationDependencyInjection
+    {
+        public static WebApplicationBuilder AddAppAuthentication(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+                    ValidAudience = builder.Configuration["JWT:ValidAudience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]!))
+                };
+            });
+
+            return builder;
+        }
+    }
+}
---
+++ MBS.API/Extensions/SwaggerDependencyInjection.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace MBS.API.Extensions
+{
+    public static class SwaggerDependencyInjection
+    {
+        public static WebApplicationBuilder AddSwaggerGen(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition(name: "Bearer", securityScheme: new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                    Description = "Please enter your token with this format: \"Bearer YOUR_TOKEN\"",
+                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme()
+                    {
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                        Reference = new OpenApiReference()
+                        {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    },
+                    new List<string>()
+                }
+            });
+            });
+
+            return builder;
+        }
+    }
+}
