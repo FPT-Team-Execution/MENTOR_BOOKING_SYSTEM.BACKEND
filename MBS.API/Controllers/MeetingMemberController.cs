8a6a3bd15bfac5de3b89f34b9a8891033d61e14b
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-04 21:37:30 +0700

    feat: add meeting member apis [datdev] | 10042024

---
+++ MBS.API/Controllers/MeetingMemberController.cs
@@ -0,0 +1,50 @@
+using MBS.Application.Models.Meeting;
+using MBS.Application.Models.MeetingMember;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MBS.API.Controllers;
+
+[ApiController]
+[Route("api/meeting-members")]
+public class MeetingMemberController : ControllerBase
+{
+    private readonly IMeetingMemberService _meetingMemberService;
+
+    public MeetingMemberController(IMeetingMemberService meetingMemberService)
+    {
+        _meetingMemberService = meetingMemberService;
+    }
+    
+    [HttpGet("meeting/{meetingId}")]
+    [ProducesResponseType(typeof(BaseModel<MeetingResponseModel>),StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(BaseModel),StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetMeetingMembersByMeetingId(Guid meetingId)
+    {
+        var result = await _meetingMemberService.GetMembersByMeetingId(meetingId);
+        return StatusCode(result.StatusCode, result);
+        
+    }
+    [HttpPost("")]
+    [ProducesResponseType(typeof(BaseModel<MeetingResponseModel>),StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(BaseModel),StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(BaseModel),StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(BaseModel),StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> AddMemberMeeting(CreateMeetingMemberRequestModel requestModel)
+    {
+        var result = await _meetingMemberService.AddMemberMeeting(requestModel);
+        return StatusCode(result.StatusCode, result);
+        
+    }
+    
+    [HttpPut("{meetingMemberId}")]
+    [ProducesResponseType(typeof(BaseModel<MeetingResponseModel>),StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(BaseModel),StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(BaseModel),StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(BaseModel),StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> UpdateMeetingMember(Guid meetingMemberId, UpdateMeetingMemberRequestModel request)
+    {
+        var result = await _meetingMemberService.UpdateMeetingMember(meetingMemberId, request);
+        return StatusCode(result.StatusCode, result);
+        
+    }
+}
