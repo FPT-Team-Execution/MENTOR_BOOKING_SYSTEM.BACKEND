72fe7dec6c544c0c8796bfdf75a09613c4626dad
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-18 01:01:42 +0700

    feat: add meeting repo [datdev] | 10182024

---
+++ MBS.DataAccess/Repositories/Implements/MeetingRepository.cs
@@ -0,0 +1,10 @@
+using MBS.Core.Entities;
+using MBS.DataAccess.DAO.Interfaces;
+using MBS.DataAccess.Repositories.Interfaces;
+
+namespace MBS.DataAccess.Repositories.Implements;
+
+public class MeetingRepository(IBaseDAO<Meeting> dao) : BaseRepository<Meeting>(dao), IMeetingRepository
+{
+    
+}
