e1b0617def02eaea74312b9dccc77503f6d32c49
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-17 23:58:09 +0700

    modify: change cast type for TKey [datdev] | 10172024

--- MBS.DataAccess/Repositories/Interfaces/IBaseRepository.cs
+++ MBS.DataAccess/Repositories/Interfaces/IBaseRepository.cs
@@ -4,7 +4,7 @@ namespace MBS.DataAccess.Repositories.Interfaces;
 
 public interface IBaseRepository<T> where T: class
 {
-    Task<T> GetByIdAsync<TKey>(TKey id, string keyName) where TKey: struct;
+    Task<T> GetByIdAsync<TKey>(TKey id, string keyName) where TKey: notnull;
     Task<IEnumerable<T>> GetAllAsync();
     Task<Pagination<T>> GetPagedListAsync(int page, int size);
     Task<bool> CreateAsync(T entity);
