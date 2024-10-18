9eb7cf65f755c4633c7c643bb9b966b383b6e055
DatSung <datsung.dev@gmail.com>
2024-10-18 20:38:26 +0700

    feat: implement validation attributes for file upload

---
+++ MBS.Application/ValidationAttributes/MaxFileSize.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace MBS.Application.ValidationAttributes;
+
+public class MaxFileSize: System.ComponentModel.DataAnnotations.ValidationAttribute
+{
+    private readonly int _maxFileSize;
+    public MaxFileSize(int maxFileSize)
+    {
+        _maxFileSize = maxFileSize;
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var file = value as IFormFile;
+
+        if (file != null)
+        {
+            if (file.Length > (_maxFileSize * 1024 * 1024))
+            {
+                return new ValidationResult($"Maximum allowed file size is {_maxFileSize} MB.");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
