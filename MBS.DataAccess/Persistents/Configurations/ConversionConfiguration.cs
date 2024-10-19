154723100e7a4c487c1083d4de8ee0bd4214cc0b
Pham Hoang Minh Khoi <phamkhoi172003@gmail.com>
2024-10-19 16:56:59 +0700

    test

--- MBS.DataAccess/Persistents/Configurations/ConversionConfiguration.cs
+++ MBS.DataAccess/Persistents/Configurations/ConversionConfiguration.cs
@@ -52,6 +52,29 @@ namespace MBS.DataAccess.Persistents.Configurations
                 v => v.ToString(),
                 v => (StatusEnum)Enum.Parse(typeof(StatusEnum), v)
             );
+
+            List<Major> defaultMajor =
+                [
+                new Major
+                {
+                    Id = Guid.Parse("903b6085-4cc3-47f3-bbdd-0f8319e5aabb"),
+                    Name = "SE",
+                    Status = StatusEnum.Activated
+                },
+                new Major
+                {
+                    Id = Guid.Parse("71577eaf-ebf1-4b23-a48d-cf8561b1c7db"),
+                    Name = "SS",
+                    Status = StatusEnum.Activated
+                },
+                new Major
+                {
+                    Id = Guid.Parse("dfdb83a4-18e0-447e-9ec8-7c8b39ee6f3a"),
+                    Name = "SA",
+                    Status = StatusEnum.Activated
+                }
+                ];
+            builder.HasData(defaultMajor);
         }
     }
     public class PositionConfiguration : IEntityTypeConfiguration<Position>
