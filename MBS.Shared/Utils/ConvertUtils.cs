415d63b826745cea645bdb627b38451f8c77ef9a
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-25 22:03:28 +0700

    update

--- MBS.Shared/Utils/ConvertUtils.cs
+++ MBS.Shared/Utils/ConvertUtils.cs
@@ -6,4 +6,25 @@ public class ConvertUtils
     {
         return string.Join(',', roles);
     }
+    public static (DateTime Start, DateTime End) GetStartEndTime(DateTime date)
+    {
+        // Start of the day: 00:00:00
+        DateTime start = date.Date;
+
+        // End of the day: 23:59:59.999
+        DateTime end = date.Date.AddDays(1).AddMilliseconds(-1);
+
+        return (start, end);
+    }
+    public static (DateTime Start, DateTime End) GetStartEndTime(DateOnly date)
+    {
+        // Start of the day: 00:00:00
+        DateTime start = date.ToDateTime(TimeOnly.MinValue);
+
+        // End of the day: 23:59:59.999
+        DateTime end = date.ToDateTime(TimeOnly.MaxValue);
+
+        return (start, end);
+    }
+
 }
