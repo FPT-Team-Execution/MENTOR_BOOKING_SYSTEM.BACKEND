4e1891fa11c25c642bb9cb82cd9bcd21433fa4c7
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-26 15:32:23 +0700

    update: add catch argument exception [datdev] | 10262024

---
+++ MBS.Application/ValidationAttributes/EnumValidation.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MBS.Application.ValidationAttributes;
+
+public class EnumValidation : ValidationAttribute
+{
+    private readonly Type _enumType;
+
+    public EnumValidation(Type enumType)
+    {
+        if (!enumType.IsEnum)
+            throw new ArgumentException("T must be an enum type");
+        _enumType = enumType;
+    }
+
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value == null || !Enum.IsDefined(_enumType, value))
+        {
+            return new ValidationResult($"Invalid value for {validationContext.DisplayName}");
+        }
+        return ValidationResult.Success;
+    }
+}
