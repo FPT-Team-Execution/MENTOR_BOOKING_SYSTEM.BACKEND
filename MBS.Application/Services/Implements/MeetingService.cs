7f5bb051c9e19834500f68cff0a76a3b2e32fadc
DatSung <datsung.dev@gmail.com>
2024-10-27 13:28:45 +0700

    feat: implement student point change after meeting created

--- MBS.Application/Services/Implements/MeetingService.cs
+++ MBS.Application/Services/Implements/MeetingService.cs
@@ -3,12 +3,14 @@ using MBS.Application.Helpers;
 using MBS.Application.Models.General;
 using MBS.Application.Models.Meeting;
 using MBS.Application.Models.PointTransaction;
+using MBS.Application.Models.Student;
 using MBS.Application.Services.Interfaces;
 using MBS.Core.Common.Pagination;
 using MBS.Core.Entities;
 using MBS.Core.Enums;
 using MBS.DataAccess.Repositories.Interfaces;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 
 namespace MBS.Application.Services.Implements;
@@ -17,22 +19,30 @@ public class MeetingService : BaseService2<MeetingService>, IMeetingService
 {
     private readonly IMeetingRepository _meetingRepository;
     private readonly IRequestRepository _requestRepository;
-    private readonly IPointTransactionSerivce _pointTransactionSerivce;
+    private readonly IPointTransactionSerivce _pointTransactionService;
+    private readonly IProjectRepository _projectRepository;
+    private readonly IGroupRepository _groupRepository;
+
     public MeetingService(
         IRequestRepository requestRepository,
         IMeetingRepository meetingRepository,
-        ILogger<MeetingService> logger, IMapper mapper, IPointTransactionSerivce pointTransactionSerivce) : base(logger, mapper)
+        ILogger<MeetingService> logger, IMapper mapper, IPointTransactionSerivce pointTransactionService,
+        IProjectRepository projectRepository, IGroupRepository groupRepository) : base(logger,
+        mapper)
     {
         _meetingRepository = meetingRepository;
-        _pointTransactionSerivce = pointTransactionSerivce;
+        _pointTransactionService = pointTransactionService;
+        _projectRepository = projectRepository;
+        _groupRepository = groupRepository;
         _requestRepository = requestRepository;
     }
+
     public async Task<BaseModel<MeetingResponseModel>> GetMeetingId(Guid meetingId)
     {
         try
         {
             var meeting = await _meetingRepository.GetByIdAsync(meetingId, "Id");
-            if(meeting == null)
+            if (meeting == null)
                 return new BaseModel<MeetingResponseModel>
                 {
                     Message = MessageResponseHelper.MeetingNotFound(meetingId.ToString()),
@@ -85,39 +95,42 @@ public class MeetingService : BaseService2<MeetingService>, IMeetingService
         }
     }
 
-    public async Task<BaseModel<CreateMeetingResponseModel, CreateMeetingRequestModel>> CreateMeeting(CreateMeetingRequestModel request)
+    public async Task<BaseModel<CreateMeetingResponseModel, CreateMeetingRequestModel>> CreateMeeting(
+        CreateMeetingRequestModel request)
     {
         try
         {
             //check request
             var requestCheck = await _requestRepository.GetByIdAsync(request.RequestId, "Id");
-            if(requestCheck == null)
+            if (requestCheck == null)
                 return new BaseModel<CreateMeetingResponseModel, CreateMeetingRequestModel>
                 {
                     Message = MessageResponseHelper.RequestNotFound(request.RequestId.ToString()),
                     IsSuccess = false,
                     StatusCode = StatusCodes.Status404NotFound,
                 };
-            
-            if(requestCheck.Status != RequestStatusEnum.Accepted)
+
+            if (requestCheck.Status != RequestStatusEnum.Accepted)
                 return new BaseModel<CreateMeetingResponseModel, CreateMeetingRequestModel>
                 {
-                    Message = MessageResponseHelper.InvalidRequestStatus(request.RequestId.ToString(), nameof(RequestStatusEnum.Accepted)),
+                    Message = MessageResponseHelper.InvalidRequestStatus(request.RequestId.ToString(),
+                        nameof(RequestStatusEnum.Accepted)),
                     IsSuccess = false,
                     StatusCode = StatusCodes.Status400BadRequest,
                 };
+
             //Create meeting
             var newMeeting = new Meeting()
             {
                 Id = Guid.NewGuid(),
-                RequestId = request.RequestId, 
+                RequestId = request.RequestId,
                 Description = request.Description,
                 Location = request.Location,
                 MeetUp = request.MeetUp,
                 Status = MeetingStatusEnum.New
             };
-            var addResult =  await _meetingRepository.CreateAsync(newMeeting);
-            if(addResult)
+            var addResult = await _meetingRepository.CreateAsync(newMeeting);
+            if (addResult)
                 return new BaseModel<CreateMeetingResponseModel, CreateMeetingRequestModel>
                 {
                     Message = MessageResponseHelper.GetSuccessfully("meeting"),
@@ -125,11 +138,44 @@ public class MeetingService : BaseService2<MeetingService>, IMeetingService
                     StatusCode = StatusCodes.Status200OK,
                     RequestModel = request,
                     ResponseModel = new CreateMeetingResponseModel
-					{
+                    {
                         RequestId = newMeeting.Id,
                     }
                 };
-            
+
+            switch (requestCheck.ProjectId)
+            {
+                case null:
+                {
+                    var transactionResult = await _pointTransactionService.ModifyStudentPoint(
+                        new ModifyStudentPointRequestModel()
+                        {
+                            Amout = 100,
+                            StudentId = requestCheck.CreaterId,
+                            TransactionType = TransactionTypeEnum.Debit
+                        });
+                    break;
+                }
+                default:
+                {
+                    var project = await _projectRepository.GetByIdAsync(requestCheck.ProjectId, "Id");
+                    var groups = await _groupRepository.GetGroupByProjectIdAsync(project.Id);
+
+                    foreach (var group in groups)
+                    {
+                        var transactionResult = await _pointTransactionService.ModifyStudentPoint(
+                            new ModifyStudentPointRequestModel()
+                            {
+                                Amout = 100,
+                                StudentId = group.StudentId,
+                                TransactionType = TransactionTypeEnum.Debit
+                            });
+                    }
+
+                    break;
+                }
+            }
+
 
             return new BaseModel<CreateMeetingResponseModel, CreateMeetingRequestModel>
             {
@@ -149,7 +195,7 @@ public class MeetingService : BaseService2<MeetingService>, IMeetingService
         }
     }
 
-    public async Task<BaseModel<MeetingResponseModel>> UpdateMeeting(Guid meetingId,UpdateMeetingRequestModel request)
+    public async Task<BaseModel<MeetingResponseModel>> UpdateMeeting(Guid meetingId, UpdateMeetingRequestModel request)
     {
         try
         {
@@ -161,7 +207,6 @@ public class MeetingService : BaseService2<MeetingService>, IMeetingService
                     Message = MessageResponseHelper.MeetingNotFound(meetingId.ToString()),
                     IsSuccess = false,
                     StatusCode = StatusCodes.Status404NotFound,
-                    
                 };
             if (meeting.Status != MeetingStatusEnum.New || meeting.Status != MeetingStatusEnum.Delayed)
                 return new BaseModel<MeetingResponseModel>
@@ -169,9 +214,8 @@ public class MeetingService : BaseService2<MeetingService>, IMeetingService
                     Message = MessageResponseHelper.InvalidMeetingSatus(meetingId.ToString()),
                     IsSuccess = false,
                     StatusCode = StatusCodes.Status400BadRequest,
-                    
                 };
-            
+
             //Update request
             meeting.Description = request.Description;
             meeting.Location = request.Location;
