9cbb7579724b39a74734937be84503e62994efdc
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-30 16:45:30 +0700

    feat: create GetRequestByProjectIdModel [datdev] | 10302024

---
+++ MBS.Application/Models/Request/GetRequestByProjectIdModel.cs
@@ -0,0 +1,21 @@
+using MBS.Application.ValidationAttributes;
+using MBS.Core.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MBS.Application.Models.Request;
+
+public class GetRequestByProjectIdModel
+{
+    [FromRoute(Name = "projectId")]
+    public required string ProjectId { get; set; }
+    [FromQuery]
+    public int Page { get; set; } = 1;
+    [FromQuery]
+    public int Size { get; set; } = 10;
+    [FromQuery]
+    public required string SortOrder { get; set; } = "asc";
+    [EnumValidation(typeof(RequestStatusEnum))]
+    public string? RequestStatus { get; set; }
+    [EnumValidation(typeof(ProjectStatusEnum))]
+    public string? ProjectStatus { get; set; }
+}
