ee5a6a737e12a3c2ecdfd6c361c3fdd009272d0d
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-30 22:42:30 +0700

    feat: create GetProjectsByMentorId in repo [datdev] | 10302024

--- MBS.DataAccess/Repositories/Interfaces/IProjectRepository.cs
+++ MBS.DataAccess/Repositories/Interfaces/IProjectRepository.cs
@@ -1,8 +1,9 @@
+using MBS.Core.Common.Pagination;
 using MBS.Core.Entities;
 
 namespace MBS.DataAccess.Repositories.Interfaces;
 
 public interface IProjectRepository : IBaseRepository<Project>
 {
-    
+    Task<Pagination<Project>> GetProjectsByMentorId(string mentorId, int page, int size, string sortOrder);
 }
