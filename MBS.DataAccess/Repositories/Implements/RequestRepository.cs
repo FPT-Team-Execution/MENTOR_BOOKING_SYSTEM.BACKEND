cb4f9ab5a81a3ab57e55f8c6a7569d17f3fcdeac
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-30 16:54:38 +0700

    feat: ceraet GetRequestByProjectdPaginationAsync in repo [datdev] | 10302024

--- MBS.DataAccess/Repositories/Implements/RequestRepository.cs
+++ MBS.DataAccess/Repositories/Implements/RequestRepository.cs
@@ -10,6 +10,17 @@ namespace MBS.DataAccess.Repositories.Implements;
 
 public class RequestRepository(IBaseDAO<Request> dao) : BaseRepository<Request>(dao), IRequestRepository
 {
+    public async Task<Pagination<Request>> GetRequestByProjectdPaginationAsync(Guid projectId, int page, int size, string sortOrder, string? requestStatus)
+    {
+        Expression<Func<Request, bool>> filter = x => x.ProjectId == projectId 
+                                                      && (!string.IsNullOrEmpty(requestStatus) && x.Status == Enum.Parse<RequestStatusEnum>(requestStatus, true)); ;
+        return await _dao.GetPagingListAsync(
+            predicate: filter,
+            orderBy: o => (sortOrder.ToLower() == "asc") ? o.OrderBy(x => x.CreatedOn) : o.OrderByDescending(x => x.CreatedOn),
+            page : page,
+            size : size);
+    }
+
     public async Task<Pagination<Request>> GetRequestByUserIdPaginationAsync(string userId, int page, int size, string sortOrder, string? requestStatus)
     {
         Expression<Func<Request, bool>> filter = x => x.CreaterId == userId 
