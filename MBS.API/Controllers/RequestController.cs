12a494e4a05d67d102f3199345f834b1b30926a4
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-30 16:37:50 +0700

    feat: get request by userid api [datdev] | 10302024

--- MBS.API/Controllers/RequestController.cs
+++ MBS.API/Controllers/RequestController.cs
@@ -1,5 +1,6 @@
 using MBS.Application.Models.Request;
 using MBS.Application.ValidationAttributes;
+using MBS.Core.Common.Pagination;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MBS.API.Controllers;
@@ -14,10 +15,21 @@ public class RequestController : ControllerBase
     {
        _requestService = requestService;   
     }
-    [HttpGet]
+    [HttpGet("user/{userId}")]
     [ProducesResponseType(typeof(BaseModel<RequestResponseModel>),StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(BaseModel),StatusCodes.Status500InternalServerError)]
-    public async Task<ActionResult<BaseModel<RequestResponseModel>>> GetRequest([FromQuery]GetRequestsPaginationRequest request)
+    [ProducesResponseType(typeof(BaseModel),StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetRequestByUserId([FromQuery]GetRequestByUserIdPaginationRequest request)
+    {
+        var result = await _requestService.GetRequestsByUserId(request);
+        return StatusCode(result.StatusCode, result);
+        
+    }
+    
+    [HttpGet("")]
+    [ProducesResponseType(typeof(BaseModel<RequestResponseModel>),StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(BaseModel),StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetRequests([FromQuery]GetRequestsPaginationRequest request)
     {
         var result = await _requestService.GetRequests(request);
         return StatusCode(result.StatusCode, result);
