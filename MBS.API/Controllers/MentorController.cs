e2f577b72f20659225d98668129df6c74640e9b1
DatSung <datsung.dev@gmail.com>
2024-10-31 11:33:52 +0700

    feat: get mentor degrees for admin

--- MBS.API/Controllers/MentorController.cs
+++ MBS.API/Controllers/MentorController.cs
@@ -6,85 +6,93 @@ using MBS.Core.Common.Pagination;
 
 namespace MBS.API.Controllers
 {
-    [Route("api/mentors")]
-    [ApiController]
-    public class MentorController : ControllerBase
-    {
-        private readonly IMentorService _mentorService;
+	[Route("api/mentors")]
+	[ApiController]
+	public class MentorController : ControllerBase
+	{
+		private readonly IMentorService _mentorService;
 
-        public MentorController(IMentorService mentorService)
-        {
-            _mentorService = mentorService;
-        }
+		public MentorController(IMentorService mentorService)
+		{
+			_mentorService = mentorService;
+		}
 
-        [HttpGet]
-        [Route("profile")]
-        [Authorize(Roles = nameof(UserRoleEnum.Mentor))]
-        public async Task<ActionResult<BaseModel<GetMentorResponseModel>>>
-            GetOwnProfile()
-        {
-            var response = await _mentorService.GetOwnProfile(User);
-            return StatusCode(response.StatusCode, response);
-        }
+		[HttpGet]
+		[Route("profile")]
+		[Authorize(Roles = nameof(UserRoleEnum.Mentor))]
+		public async Task<ActionResult<BaseModel<GetMentorResponseModel>>>
+			GetOwnProfile()
+		{
+			var response = await _mentorService.GetOwnProfile(User);
+			return StatusCode(response.StatusCode, response);
+		}
 
-        [HttpPost]
-        [Route("degree")]
-        [Authorize(Roles = nameof(UserRoleEnum.Mentor))]
-        public async Task<ActionResult<BaseModel<UploadOwnDegreeResponseModel, UploadOwnDegreeRequestModel>>>
-            UploadOwnDegree(UploadOwnDegreeRequestModel request)
-        {
-            var response = await _mentorService.UploadOwnDegree(request, User);
-            return StatusCode(response.StatusCode, response);
-        }
+		[HttpPost]
+		[Route("degree")]
+		[Authorize(Roles = nameof(UserRoleEnum.Mentor))]
+		public async Task<ActionResult<BaseModel<UploadOwnDegreeResponseModel, UploadOwnDegreeRequestModel>>>
+			UploadOwnDegree(UploadOwnDegreeRequestModel request)
+		{
+			var response = await _mentorService.UploadOwnDegree(request, User);
+			return StatusCode(response.StatusCode, response);
+		}
 
-        [HttpGet]
-        [Route("degree")]
-        [Authorize(Roles = nameof(UserRoleEnum.Mentor))]
-        public async Task<ActionResult<BaseModel<GetOwnDegreesResponseModel>>>
-            GetOwnDegrees(int page, int size)
-        {
-            var response = await _mentorService.GetOwnDegrees(User, page, size);
-            return StatusCode(response.StatusCode, response);
-        }
+		[HttpGet]
+		[Route("degree")]
+		[Authorize(Roles = nameof(UserRoleEnum.Mentor))]
+		public async Task<ActionResult<BaseModel<GetOwnDegreesResponseModel>>>
+			GetOwnDegrees(int page, int size)
+		{
+			var response = await _mentorService.GetOwnDegrees(User, page, size);
+			return StatusCode(response.StatusCode, response);
+		}
 
-        [HttpGet]
-        [Route("{id}")]
-        [CustomAuthorize(UserRoleEnum.Admin, UserRoleEnum.Student)]
-        public async Task<IActionResult> GetMentor([FromRoute] string id)
-        {
-            var response = await _mentorService.GetMentor(new GetMentorRequestModel()
-            {
-                Id = id
-            });
-            return StatusCode(response.StatusCode, response);
-        }
+		[HttpGet]
+		[Route("{id}")]
+		[CustomAuthorize(UserRoleEnum.Admin, UserRoleEnum.Student)]
+		public async Task<IActionResult> GetMentor([FromRoute] string id)
+		{
+			var response = await _mentorService.GetMentor(new GetMentorRequestModel()
+			{
+				Id = id
+			});
+			return StatusCode(response.StatusCode, response);
+		}
 
-        [HttpGet]
-        [Authorize(Roles = nameof(UserRoleEnum.Admin))]
-        [ProducesResponseType(typeof(BaseModel<Pagination<GetMentorResponseModel>>), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(BaseModel), StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetMentors(int page, int size)
-        {
-            var response = await _mentorService.GetMentors(page, size);
-            return StatusCode(response.StatusCode, response);
-        }
+		[HttpGet]
+		[Authorize(Roles = nameof(UserRoleEnum.Admin))]
+		[ProducesResponseType(typeof(BaseModel<Pagination<GetMentorResponseModel>>), StatusCodes.Status200OK)]
+		[ProducesResponseType(typeof(BaseModel), StatusCodes.Status500InternalServerError)]
+		public async Task<IActionResult> GetMentors(int page, int size)
+		{
+			var response = await _mentorService.GetMentors(page, size);
+			return StatusCode(response.StatusCode, response);
+		}
 
-        [HttpGet]
-        [Route("search/{searchItem}")]
-        //[Authorize(Roles = nameof(UserRoleEnum.Mentor))]
-        public async Task<ActionResult<BaseModel<List<MentorSearchDTO>>>>
-            MentorSearch(string searchItem)
-        {
-            var response = await _mentorService.SearchMentor(searchItem);
-            return StatusCode(response.StatusCode, response);
-        }
+		[HttpGet]
+		[Route("search/{searchItem}")]
+		//[Authorize(Roles = nameof(UserRoleEnum.Mentor))]
+		public async Task<ActionResult<BaseModel<List<MentorSearchDTO>>>>
+			MentorSearch(string searchItem)
+		{
+			var response = await _mentorService.SearchMentor(searchItem);
+			return StatusCode(response.StatusCode, response);
+		}
 
-        [HttpPut]
-        [Route("profile")]
-        public async Task<IActionResult> UpdateMentor(UpdateMentorRequestModel request)
-        {
-            var response = await _mentorService.UpdateOwnProfile(User, request);
-            return StatusCode(response.StatusCode, response);
-        }
-    }
+		[HttpPut]
+		[Route("profile")]
+		public async Task<IActionResult> UpdateMentor(UpdateMentorRequestModel request)
+		{
+			var response = await _mentorService.UpdateOwnProfile(User, request);
+			return StatusCode(response.StatusCode, response);
+		}
+
+		[HttpGet]
+		[Route("degree-display")]
+		public async Task<IActionResult> GetMentorDegrees([FromQuery] GetMentorDegreesRequestModel request)
+		{
+			var response = await _mentorService.GetMentorDegrees(request);
+			return StatusCode(response.StatusCode, response);
+		}
+	}
 }
---
+++ MBS.Application/Models/Mentor/GetMentorDegreesModel.cs
@@ -0,0 +1,32 @@
+﻿using MBS.Application.Models.User;
+using MBS.Core.Common.Pagination;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBS.Application.Models.Mentor
+{
+	public class GetMentorDegreesRequestModel
+	{
+		public required string MentorId { get; set; }
+		public required int Page { get; set; }
+		public required int Size { get; set; }
+	}
+
+	public class GetMentorDegreeResponseModel
+	{
+		public required Guid Id { get; set; }
+		public string Name { get; set; } = default;
+		public string? ImageUrl { get; set; } = default;
+		public string? Institution { get; set; } = default;
+
+	}
+
+	public class GetMentorDegreesResponseModel
+	{
+		public required Pagination<GetMentorDegreeResponseModel> Degrees;
+
+	}
+}
