d746f272ed85f90d08019006c26179969211d123
DatSung <datsung.dev@gmail.com>
2024-11-03 15:39:44 +0700

    feat: add replace option for supabase

--- MBS.Shared/Services/Implements/SupabaseService.cs
+++ MBS.Shared/Services/Implements/SupabaseService.cs
@@ -11,33 +11,33 @@ namespace MBS.Shared.Services.Implements;
 
 public class SupabaseService : ISupabaseService
 {
-    private readonly ISupabaseClient<User, Session, RealtimeSocket, RealtimeChannel, Bucket, FileObject>
-        _supabaseClient;
+	private readonly ISupabaseClient<User, Session, RealtimeSocket, RealtimeChannel, Bucket, FileObject>
+		_supabaseClient;
 
-    public SupabaseService(
-        ISupabaseClient<User, Session, RealtimeSocket, RealtimeChannel, Bucket, FileObject> supabaseClient)
-    {
-        _supabaseClient = supabaseClient;
-    }
+	public SupabaseService(
+		ISupabaseClient<User, Session, RealtimeSocket, RealtimeChannel, Bucket, FileObject> supabaseClient)
+	{
+		_supabaseClient = supabaseClient;
+	}
 
-    public async Task<string> UploadFile(byte[] fileByte, string filePath, string bucketName)
-    {
-        return await _supabaseClient.Storage.From(bucketName)
-            .Upload
-            (
-                fileByte,
-                filePath,
-                new FileOptions()
-                {
-                    Upsert = false,
-                    CacheControl = "3600",
-                    ContentType = "image",
-                }
-            );
-    }
+	public async Task<string> UploadFile(byte[] fileByte, string filePath, string bucketName, bool replace)
+	{
+		return await _supabaseClient.Storage.From(bucketName)
+			.Upload
+			(
+				fileByte,
+				filePath,
+				new FileOptions()
+				{
+					Upsert = replace,
+					CacheControl = "3600",
+					ContentType = "image",
+				}
+			);
+	}
 
-    public string RetrievePublicUrl(string bucketName, string filePath)
-    {
-        return _supabaseClient.Storage.From(bucketName).GetPublicUrl(filePath);
-    }
+	public string RetrievePublicUrl(string bucketName, string filePath)
+	{
+		return _supabaseClient.Storage.From(bucketName).GetPublicUrl(filePath);
+	}
 }
