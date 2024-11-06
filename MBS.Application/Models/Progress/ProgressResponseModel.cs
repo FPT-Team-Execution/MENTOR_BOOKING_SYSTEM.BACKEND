c6d7da8cf7a2d96c6641cc84a6ac49eaeb6e80e1
NguyenDat0809 <datngx.dev@gmail.com>
2024-11-07 00:04:10 +0700

    fix: missing id return [datdev] | 11072024

--- MBS.Application/Models/Progress/ProgressResponseModel.cs
+++ MBS.Application/Models/Progress/ProgressResponseModel.cs
@@ -4,6 +4,7 @@ namespace MBS.Application.Models.Progress;
 
 public class ProgressResponseDto
 {
+    public Guid Id{ get; set; }
     public required string Name { get; set; }
     public bool IsComplete { get; set; }
     public string? UpdatedBy { get; set; }
