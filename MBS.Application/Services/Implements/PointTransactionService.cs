1ff864f7b2ed011a46c563dc93b3a333745036cb
Pham Hoang Minh Khoi <phamkhoi172003@gmail.com>
2024-11-07 15:32:48 +0700

    pagination count return

--- MBS.Application/Services/Implements/PointTransactionService.cs
+++ MBS.Application/Services/Implements/PointTransactionService.cs
@@ -152,7 +152,8 @@ namespace MBS.Application.Services.Implements
             {
                 Items = response,
                 PageSize = size,
-                PageIndex = page
+                PageIndex = page,
+                TotalItems = result.Count(),
 
             };
             return new BaseModel<Pagination<PointTransactionDTO>>
