f777687dba1b70a36a130cdb8843292699b6355e
DatSung <datsung.dev@gmail.com>
2024-11-09 11:12:35 +0700

    Update PointTransactionService.cs

--- MBS.Application/Services/Implements/PointTransactionService.cs
+++ MBS.Application/Services/Implements/PointTransactionService.cs
@@ -149,7 +149,7 @@ namespace MBS.Application.Services.Implements
                 };
                 ListToShow.Add(newTrans);
             }
-            var response = ListToShow.OrderBy(i => i.CreatedOn).ToList();
+            var response = ListToShow.OrderByDescending(i => i.CreatedOn).ToList();
 
             var pagingPoint = new Pagination<PointTransactionDTO>
             {
@@ -191,7 +191,7 @@ namespace MBS.Application.Services.Implements
 
             var paginatedDtoList = new Pagination<PointTransactionDTO>
             {
-                Items = transactionDtoList,
+                Items = transactionDtoList.OrderByDescending(x=> x.CreatedOn),
                 PageIndex = page,
                 PageSize = size,
                 TotalItems = result.TotalItems,
